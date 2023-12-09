14b7086bc8f9b6b38a8dab37bfe3ae2f1b0c099c
r0bcodes <152415321+r0bcodes@users.noreply.github.com>
2023-12-09 01:43:12 +0100

    Updated Jucer, added FilterGraph for easy plugin debug

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -27,7 +27,7 @@ public:
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    PFMProject0AudioProcessor& audioProcessor;
+    PFMProject0AudioProcessor& audioProcessor; 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PFMProject0AudioProcessorEditor)
 };
